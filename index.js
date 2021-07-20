00a21881d4b2ca3d7210c3626ab0b5bf898ea113
Morvicek <65585242+Morvicek@users.noreply.github.com>
2021-07-20 10:21:33 +0200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @author      Created by Morvicek on 2021-07-16.
- * @link        none
- * @license     http://opensource.org/licenses/MIT
- */
-
-module.exports = require("./djs-lang-main") 
